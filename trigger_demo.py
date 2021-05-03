7a0fc5ecc7b3948cd5b9e550367c1142ee2c5051
Dudemullet <elpaip@gmail.com>
2021-05-03 07:39:42 -0700

    Adds visibility setting

--- trigger_demo.py
+++ trigger_demo.py
@@ -1,6 +1,8 @@
 import uuid
 app_name = "quest"
-INFLIGHT_KEYS = "quest:inflight"
+INFLIGHT_KEYS = "inflight"
+KEY_SEPARATOR = ":"
+DEFAULT_VISIBILITY_TIMEOUT = 5
 
 def add_command(arguments):
     command, list_name, value = arguments
@@ -10,20 +12,37 @@ def add_command(arguments):
     items_dictionary = {
             "uuid": item_identifier,
             "value": value,
-            "tries": 0
+            "tries": 0,
+            "in_flight": False,
             }
     items_list = list(sum(items_dictionary.items(), tuple()))
     execute("HSET", f"{app_name}:{item_identifier}", *items_list)
 
-def incr_and_get_message(item):
-    execute("HINCRBY", f'{app_name}:{item}', 'tries', 1)
-    return execute("HGETALL", f'{app_name}:{item}')
-
-
 def getMessage_command(arguments):
-    command, list_name, count = arguments
+    command, list_name, count, *visibility = arguments
+
+    if len(visibility) == 0:
+        visibility = DEFAULT_VISIBILITY_TIMEOUT
+    else:
+        visibility = int(visibility[0])
+
     results = execute("lrange", list_name, int(0), int(count)-1)
-    return list(map(incr_and_get_message, results))
+    return list(map(send_to_in_flight(visibility), results))
+
+def send_to_in_flight(timeout):
+    def __process(item):
+        execute("HINCRBY", f'{app_name}:{item}', 'tries', 1)
+        execute("HSET", f'{app_name}:{item}', 'in_flight', True)
+        execute("SETEX", f'{app_name}:{INFLIGHT_KEYS}:{item}', str(timeout), None)
+        return execute("HGETALL", f'{app_name}:{item}')
+
+    return __process
+
+def un_flight_a_message(item):
+    log(str(item), level='warning')
+    one, two, item_id = item['key'].rsplit(KEY_SEPARATOR)
+    log(f'Item id: {item_id}', level='warning')
+    execute("HSET", f'{app_name}:{item_id}', 'in_flight', False)
 
 gb = GB('CommandReader')
 gb.foreach(add_command)
@@ -32,3 +51,9 @@ gb.register(trigger='add')
 get_gb = GB('CommandReader')
 get_gb.flatmap(getMessage_command)
 get_gb.register(trigger='getMessage')
+
+expire_gb = GB('KeysReader')
+expire_gb.foreach(un_flight_a_message)
+expire_gb.register(prefix=f'{app_name}:{INFLIGHT_KEYS}:*',
+             eventTypes=['expired'],
+             readValue=False)
