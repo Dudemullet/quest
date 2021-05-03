81568ad43846b67cad5798261ac7109682d2ca6d
Dudemullet <elpaip@gmail.com>
2021-05-02 22:02:20 -0700

    Gets items and increemtns tries

--- trigger_demo.py
+++ trigger_demo.py
@@ -1,5 +1,6 @@
 import uuid
-app_name = "tempo"
+app_name = "quest"
+INFLIGHT_KEYS = "quest:inflight"
 
 def add_command(arguments):
     command, list_name, value = arguments
@@ -12,8 +13,19 @@ def add_command(arguments):
             "tries": 0
             }
     items_list = list(sum(items_dictionary.items(), tuple()))
-    execute("HSET", f"tempo:{item_identifier}", *items_list)
+    execute("HSET", f"{app_name}:{item_identifier}", *items_list)
+
+def getMessage_command(arguments):
+    command, list_name, count = arguments
+    results = execute("lrange", list_name, int(0), int(count)-1)
+    for item in results:
+        execute("HINCRBY", f'{app_name}:{item}', 'tries', 1)
+    return results
 
 gb = GB('CommandReader')
 gb.foreach(add_command)
 gb.register(trigger='add')
+
+get_gb = GB('CommandReader')
+get_gb.map(getMessage_command)
+get_gb.register(trigger='getMessage')
