73959f3ac2d00372fb5709b2de7fd61dedc4d9fb
Dudemullet <elpaip@gmail.com>
2021-05-14 06:27:38 -0700

    Deletes unused demo file

--- log_demo.py
+++
@@ -1,9 +0,0 @@
-def cas(x):
-    log(str(x), level='warning')
-
-# Event handling function registration
-gb = GearsBuilder()
-gb.foreach(cas)
-gb.register('person:*', convertToStr=True)
-
-## Expected result: ['OK']
